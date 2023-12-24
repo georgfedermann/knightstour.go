7f9e7cbff61f0dc2a409179d2ec1d1237ef87a8d
georg.federmann-external <georg.federmann-external@rbinternational.com>
2023-12-24 16:08:26 +0100

    Add command-line arguments for start position and verbosity
    This commit introduces command-line argument parsing to the
    knight's tour program, allowing the user to specify the starting
    position of the knight with -x and -y flags.
    
    Additionally, a -v flag has been added to enable verbose output,
    which includes move numbers and board states. The internal function
    'findPath' has been renamed to 'FindPath' to follow Go's convention
    for exported functions.
    
    The 'printBoards' function now uses fixed-width formatting for
    enhanced readability of the board's output.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
@@ -18,13 +19,15 @@ var KnightMoves [8]KnightMove = [8]KnightMove{
 	{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2},
 }
 
+var verbose bool = false
+
 func printBoards(board1 Board, board2 Board) {
-	for row := 0; row < len(board1); row++ {
-		for col := 0; col < len(board1[0]); col++ {
-			fmt.Printf("%d ", board1[row][col])
+	for row := 0; row < 8; row++ {
+		for col := 0; col < 8; col++ {
+			fmt.Printf("%02d ", board1[row][col])
 		}
 		fmt.Print("   ")
-		for col := 0; col < len(board2[0]); col++ {
+		for col := 0; col < 8; col++ {
 			fmt.Printf("%d ", board2[row][col])
 		}
 		fmt.Println()
@@ -77,34 +80,59 @@ func applyMove(position Position, move KnightMove) Position {
 	return Position{X: position.X + move.Dx, Y: position.Y + move.Dy}
 }
 
-func findPath(currentBoard Board, currentPosition Position, moveNumber int) []Position {
+// FindPath attempts to find a path for a knight on a chessboard that visits
+// every sqaure exactly once using Warndorff's rule in a backtracking fashion.
+// It is a recursive backtracking algorithm where each call attempts to find
+// the next move.
+// currentBoard is the current state of the board with the positions already
+// visited marked with the move number. This way, the algorithm can also try
+// to solve a position that has already been partially solved.
+// currentPosition is the current position of the knight on the board.
+// moveNumber is the current move number. It is used to determine if the
+// board is solved.
+// The function returns a slice of Position that represents the path of the knight.
+// If not path is found, an empty slice is returned. At this point, the calling
+// function should backtrack.
+func FindPath(currentBoard Board, currentPosition Position, moveNumber int) []Position {
+	// Mark the current position on the board with the move number
 	currentBoard[currentPosition.Y][currentPosition.X] = moveNumber
+	// Calculate the accessibility heuristics for the current board
 	heuristics := calculateHeuristics(currentBoard)
-	fmt.Printf("Move %d\n", moveNumber)
-	printBoards(currentBoard, heuristics)
-
+	// Deebugging output showing the current move and the state of the board
+	// and the heuristics
+	if verbose {
+		fmt.Printf("Move %d -> %v\n", moveNumber, currentPosition)
+		printBoards(currentBoard, heuristics)
+	}
+	// If the movenumber is 64, we have visited every square and hence found a full path
 	if moveNumber == 64 {
+		// Add the currentPosition to return the full path
 		return []Position{currentPosition}
 	} else {
+		// If we have not finished yet, iterate over all possible moves prioritized
+		// by the accessibility heuristics
 		for _, move := range warndorffSortByAccessibility(KnightMoves, currentBoard, currentPosition) {
-			subPath := findPath(currentBoard, applyMove(currentPosition, move), moveNumber+1)
+			// Recursively call FindPath with the new position and the next move number
+			subPath := FindPath(currentBoard, applyMove(currentPosition, move), moveNumber+1)
+			// If a subpath is found, append the current position and return the path
 			if len(subPath) > 0 {
 				return append([]Position{currentPosition}, subPath...)
 			}
 		}
 	}
+	// If no move leads to a solution, return an empty path indicating failure to
+	// find a complete path. The calling function should now backtrack.
 	return []Position{}
 }
 
 func main() {
-	var board Board
-	heuristics := calculateHeuristics(board)
+	xPtr := flag.Int("x", 0, "The X position (column) to start the knight's tour at")
+	yPtr := flag.Int("y", 0, "The Y position (row) to start the knight's tour at")
+	verbosePtr := flag.Bool("v", false, "Enable verbose output")
+    flag.Parse()
+	verbose = *verbosePtr
 
-	printBoards(board, heuristics)
-	fmt.Println(KnightMoves)
-	fmt.Println(len(KnightMoves))
-	fmt.Println(warndorffSortByAccessibility(KnightMoves, board, Position{X: 1, Y: 2}))
-	fmt.Println()
-    path := findPath(board, Position{X: 0, Y: 0}, 1)
-    fmt.Println(path)
+	var board Board
+	path := FindPath(board, Position{X: *xPtr, Y: *yPtr}, 1)
+	fmt.Println(path)
 }
